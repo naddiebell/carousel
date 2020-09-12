6243747ffdbcd026ba7f2761b37503330776dc7c
Natalie Whitton <newhitton@gmail.com>
2020-09-12 20:23:36 +0200

    test image on filmstrip

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ function App(props) {
         </div>
       </div>
       <div className="Filmstrip">
-        <div className="Box Box1"></div>
-          <img src="" />
+        <div className="Box Box1"> <img src={"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*"} /></div>
+         
         <div className="Box Box2"></div>
           <img />
         <div className="Box Box3"></div>
