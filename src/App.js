d9bd025ae86c6e780d371310140fc916aada441a
Natalie Whitton <newhitton@gmail.com>
2020-09-12 21:49:19 +0200

    deleted CatData, App fetch's api and makes array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
-import './catData';
-import CatData from './catData';
 import Filmstrip from './Filmstrip';
 
 
-function App(props) {
-  const {state, setstate, photoArray} = props;
-  console.log("state", state);
+function App() {
+
+  const [state, setState] = useState([]);
+  
+  React.useEffect(() => {
+    
+    fetch("https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=04798704e9e3c86362c64d3fe2f5eedc&gallery_id=58061135-72157626529791195&format=json&nojsoncallback=1")
+    .then(response => response.json())
+    .then(data => setState(data.photos.photo));
+    
+    },[])
+
+  /*const {state, setstate, photoArray} = props;
+  console.log("state", state);*/
 
   return (
     <div>
       <div className="Background">
-        <CatData />
         
         <div className="SearchDiv">
         </div>
